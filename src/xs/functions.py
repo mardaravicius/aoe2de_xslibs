029391e125ca734cfa698a9b450a34e81f8a8d3f
mardaravicius <45588145+mardaravicius@users.noreply.github.com>
2025-10-22 12:00:09 +0200

    implement mt random

--- src/xs/functions.py
+++ src/xs/functions.py
@@ -3,10 +3,9 @@ from numpy import int32
 from xs_converter.functions import xs_array_create_int, xs_array_set_int, xs_array_get_int
 from xs_converter.symbols import XsStatic
 
+_powers: int32 = int32(-1)
 
 def xs_bit_shift_left(x: int32, n: int32) -> int32:
-    if n == 0:
-        return x
     if n < 0 or n >= 32:
         return int32(0)
     result: int32 = x
@@ -17,9 +16,7 @@ def xs_bit_shift_left(x: int32, n: int32) -> int32:
     return result
 
 
-def xs_bit_shift_right(x: int32, n: int32) -> int32:
-    if n == 0:
-        return x
+def xs_bit_shift_arithmetic_right(x: int32, n: int32) -> int32:
     if n < 0 or n >= 32:
         if x < 0:
             return int32(-1)
@@ -32,17 +29,37 @@ def xs_bit_shift_right(x: int32, n: int32) -> int32:
     return result
 
 
+def xs_bit_shift_logical_right(x: int32, n: int32) -> int32:
+    if n < 0 or n >= 32:
+        return int32(0)
+    i: int32 = int32(0)
+    result: int32 = x
+    if x < 0:
+        result = x + int32(-2147483648)
+        while i < n:
+            result //= 2
+            i += 1
+        powers: int32 = _get_powers()
+        p: int32 = xs_array_get_int(powers, 31 - n)
+        result += p
+    else:
+        while i < n:
+            result //= 2
+            i += 1
+    return result
+
+
 def xs_bit_not(n: int32) -> int32:
     return (n * -1) - 1
 
 
-def _compute_powers() -> int32:
-    arr: XsStatic[int32] = -1
-    if arr == -1:
-        arr = xs_array_create_int(32, 1)
+def _get_powers() -> int32:
+    global _powers
+    if _powers == -1:
+        _powers = xs_array_create_int(32, 1)
         for i in range(1, 32):
-            xs_array_set_int(arr, i, int32(xs_array_get_int(arr, i - 1)) * 2)
-    return arr
+            xs_array_set_int(_powers, i, int32(xs_array_get_int(_powers, i - 1)) * 2)
+    return _powers
 
 
 def _get_bit(x: int32, i: int32, powers: int32) -> int32:
@@ -58,7 +75,7 @@ def _set_bit(bit: int32, i: int32, powers: int32) -> int32:
 
 
 def xs_bit_and(a: int32, b: int32) -> int32:
-    powers = _compute_powers()
+    powers: int32 = _get_powers()
     res: int32 = int32(0)
     for i in range(0, 31):
         abit: int32 = _get_bit(a, i, powers)
@@ -71,7 +88,7 @@ def xs_bit_and(a: int32, b: int32) -> int32:
 
 
 def xs_bit_xor(a: int32, b: int32) -> int32:
-    powers = _compute_powers()
+    powers: int32 = _get_powers()
     res: int32 = int32(0)
     for i in range(0, 31):
         abit: int32 = _get_bit(a, i, powers)
@@ -84,7 +101,7 @@ def xs_bit_xor(a: int32, b: int32) -> int32:
 
 
 def xs_bit_or(a: int32, b: int32) -> int32:
-    powers = _compute_powers()
+    powers: int32 = _get_powers()
     res: int32 = int32(0)
     for i in range(0, 31):
         abit: int32 = _get_bit(a, i, powers)
@@ -95,3 +112,104 @@ def xs_bit_or(a: int32, b: int32) -> int32:
         res += -2147483648
     return res
 
+
+def xs_unsigned_multiply(a: int32, b: int32) -> int32:
+    a_hi: int32 = xs_bit_shift_logical_right(a, int32(16))
+    a_lo: int32 = xs_bit_and(a, int32(65535))
+    b_hi: int32 = xs_bit_shift_logical_right(b, int32(16))
+    b_lo: int32 = xs_bit_and(b, int32(65535))
+
+    res_lo: int32 = a_lo * b_lo
+    res_mid: int32 = a_hi * b_lo + a_lo * b_hi
+
+    return res_lo + xs_bit_shift_left(res_mid, int32(16))
+
+
+_n: int32 = int32(624)
+_m: int32 = int32(397)
+_w: int32 = int32(32)
+_r: int32 = int32(31)
+_matrix_a: int32 = int32(-1727483681)
+_upper_mask: int32 = int32(-2147483648)
+_lower_mask: int32 = int32(2147483647)
+
+_a: int32 = int32(-1727483681)
+_u: int32 = int32(11)
+_s: int32 = int32(7)
+_t: int32 = int32(15)
+_l: int32 = int32(18)
+_b: int32 = int32(-1658038656)
+_c: int32 = int32(-272236544)
+_f: int32 = int32(1812433253)
+
+_state_array: int32 = int32(-1)
+_state_index: int32 = int32(0)
+
+
+def xs_mersenne_twister_seed(seed: int32) -> None:
+    global _state_array, _state_index, _upper_mask, _lower_mask
+    if _state_array < 0:
+        _state_array = int32(xs_array_create_int(_n, 0))
+    xs_array_set_int(_state_array, 0, seed)
+    i: int32 = int32(1)
+    while (i < _n):
+        seed = _f * xs_bit_xor(seed, xs_bit_shift_logical_right(seed, (_w - 2))) + i
+        xs_array_set_int(_state_array, i, seed)
+        i += 1
+    _state_index = int32(0)
+
+
+def xs_mersenne_twister_random() -> int32:
+    global _state_index, _state_index
+
+    k: int32 = _state_index
+
+    j: int32 = k - (_n - 1)
+    if j < 0:
+        j += _n
+
+    x: int32 = xs_bit_or(
+        xs_bit_and(xs_array_get_int(_state_array, k), _upper_mask),
+        xs_bit_and(xs_array_get_int(_state_array, j), _lower_mask),
+    )
+
+    xa: int32 = xs_bit_shift_logical_right(x, int32(1))
+    if (xs_bit_and(x, int32(1)) != 0):
+        xa = xs_bit_xor(xa, _a)
+
+    j = k - (_n - _m)
+    if j < 0:
+        j += _n
+
+    x = xs_bit_xor(xs_array_get_int(_state_array, j), xa)
+    xs_array_set_int(_state_array, k, x)
+    k += 1
+
+    if k >= _n:
+        k = int32(0)
+    _state_index = k
+
+    y: int32 = xs_bit_xor(x, xs_bit_shift_logical_right(x, _u))
+    y = xs_bit_xor(y, xs_bit_and(xs_bit_shift_left(y, _s), _b))
+    y = xs_bit_xor(y, xs_bit_and(xs_bit_shift_left(y, _t), _c))
+    z: int32 = xs_bit_xor(y, xs_bit_shift_logical_right(y, _l))
+
+    return z
+
+
+def xs_mersenne_twister_random_uniform_range(start: int32, end: int32) -> int32:
+    range: int32 = end - start
+    if range <= 0:
+        return int32(-1)
+
+    if xs_bit_and(range, (range - 1)) == 0:
+        return start + xs_bit_and(xs_mersenne_twister_random(), (range - 1))
+
+    threshold: int32 = (-1 * range) % range
+
+    while True:
+        r: int32 = xs_mersenne_twister_random()
+        unsigned_r: int32 = r + int32(-2147483648)
+        if unsigned_r >= threshold:
+            result: int32 = unsigned_r % range
+            return start + result
--- tests/functions_test.py
+++ tests/functions_test.py
@@ -1,14 +1,18 @@
 import random
 import unittest
 
-from numpy import int32
+import numpy
+from numpy import int32, uint32
 
-from xs.functions import xs_bit_shift_right, xs_bit_shift_left, xs_bit_not, xs_bit_and, xs_bit_xor, xs_bit_or
+from xs.functions import xs_bit_shift_arithmetic_right, xs_bit_shift_left, xs_bit_not, xs_bit_and, xs_bit_xor, \
+    xs_bit_or, \
+    xs_mersenne_twister_seed, xs_mersenne_twister_random, xs_bit_shift_logical_right, \
+    xs_mersenne_twister_random_uniform_range
 
 
 class FunctionsTest(unittest.TestCase):
 
-    def test_right_shift(self):
+    def test_right_shift_arithmetic(self):
         for n in range(100000):
             a = int32(random.randint(-2147483648, 2147483647))
             if random.randint(0, 1) == 1:
@@ -16,16 +20,35 @@ class FunctionsTest(unittest.TestCase):
             else:
                 b = int32(random.randint(-2147483648, 2147483647))
             expected = int32(a) >> int32(b)
-            actual = xs_bit_shift_right(int32(a), int32(b))
+            actual = xs_bit_shift_arithmetic_right(int32(a), int32(b))
             self.assertEqual(expected, actual, f"{a} >> {b}")
 
-    def test_right_shift_edges(self):
+    def test_right_shift_arithmetic_edges(self):
         edges = [-2147483648, -2147483647, 2147483647, 2147483646, 0, 1, -1, 2, -2]
         for a in edges:
             for b in list(range(-32, 64)) + edges:
                 expected = int32(a) >> int32(b)
-                actual = xs_bit_shift_right(int32(a), int32(b))
+                actual = xs_bit_shift_arithmetic_right(int32(a), int32(b))
                 self.assertEqual(expected, actual, f"{a} >> {b}")
+
+    def test_right_shift_logical(self):
+        for n in range(100000):
+            a = int32(random.randint(-2147483648, 2147483647))
+            if random.randint(0, 1) == 1:
+                b = int32(random.randint(-1, 33))
+            else:
+                b = int32(random.randint(-2147483648, 2147483647))
+            expected = int32(uint32(int32(a)) >> uint32(int32(b)))
+            actual = xs_bit_shift_logical_right(int32(a), int32(b))
+            self.assertEqual(expected, actual, f"{a} >>> {b}")
+
+    def test_right_shift_logical_edges(self):
+        edges = [-2147483648, -2147483647, 2147483647, 2147483646, 0, 1, -1, 2, -2]
+        for a in edges:
+            for b in list(range(-32, 64)) + edges:
+                expected = int32(uint32(int32(a)) >> uint32(int32(b)))
+                actual = xs_bit_shift_logical_right(int32(a), int32(b))
+                self.assertEqual(expected, actual, f"{a} >>> {b}")
 
     def test_left_shift(self):
         for n in range(100000):
@@ -107,3 +130,68 @@ class FunctionsTest(unittest.TestCase):
                 expected = int32(a) | int32(b)
                 actual = xs_bit_or(int32(a), int32(b))
                 self.assertEqual(expected, actual, f"{a} ^ {b}")
+
+    def test_unsigned_multiply(self):
+        for n in range(100000):
+            a = random.randint(0, 4294967295)
+            b = random.randint(0, 4294967295)
+            ua = uint32(a)
+            ub = uint32(b)
+            # aa = xs_unsigned_multiply(int32(ua), int32(ub))
+
+            ia = int32(ua)
+            ib = int32(ub)
+            r = ia * ib
+            actual = uint32(r)
+            expected = ua * ub
+
+            self.assertEqual(expected, actual, f"{a} * {b}")
+
+    def test_random_uniform_in_range(self):
+        xs_mersenne_twister_seed(int32(1))
+
+        for _ in range(1000):
+            s = int32(random.randint(-2147483648, 2147483647))
+            e = int32(random.randint(s, 2147483647))
+            print(f"[{s}, {e}]")
+            r = xs_mersenne_twister_random_uniform_range(s, e)
+            if s == e:
+                self.assertEqual(-1, r)
+            else:
+                self.assertGreaterEqual(r, s, f"[{s}, {e}]")
+                self.assertLess(r, e, f"[{s}, {e}]")
+
+    def test_random_uniform_is_uniform(self):
+        with numpy.errstate(over='ignore'):
+            seed = int32(random.randint(-2147483648, 2147483647))
+            xs_mersenne_twister_seed(seed)
+
+            d = random.randint(1, 200)
+            s = random.randint(-2147483648, 2147483647 - d)
+            e = random.randint(s + 1, s + d -1)
+            r = e - s
+            loops = 10000
+            avg_items = loops // r
+
+            results = {}
+            for i in range(s, e):
+                results[i] = 0
+            for _ in range(loops):
+                res = xs_mersenne_twister_random_uniform_range(int32(s), int32(e))
+                results[res] += 1
+            results = sorted(list(results.items()), key=lambda t: t[1])
+            for number, occurrences in results:
+                self.assertGreaterEqual(occurrences, avg_items * 0.67, f"{seed=}, {s=}, {e=}, {number=}, {occurrences=}")
+                self.assertLessEqual(occurrences, avg_items * 1.33, f"{seed=}, {s=}, {e=}, {number=}, {occurrences=}")
+
+    def test_random(self):
+        random.seed(1)
+        xs_mersenne_twister_seed(int32(1))
+
+        for _ in range(200):
+            r = int32(uint32(random.getrandbits(32)))
+            print(r)
+            r2 = xs_mersenne_twister_random()
+            print(r2)
+
+        self.assertEqual(1, 1)
